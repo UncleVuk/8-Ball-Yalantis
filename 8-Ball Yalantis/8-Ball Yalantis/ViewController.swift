d1c991db3d954fbd1c98c85e0b27274cf80624a1
Yevhenii Vuksta <vuksta.yevhenii@gmail.com>
2022-01-19 15:20:08 +0200

    8-Ball: Added assets, created layout

--- 8-Ball Yalantis/8-Ball Yalantis/ViewController.swift
+++ 8-Ball Yalantis/8-Ball Yalantis/ViewController.swift
@@ -17,3 +17,73 @@ class ViewController: UIViewController {
 
 }
 
+
+
+/*
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Link magic ball label
+    @IBOutlet weak var answerTextLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.becomeFirstResponder() // To get phone shake motion
+    }
+    
+    // Become first responder to get shake motion
+    override var canBecomeFirstResponder: Bool {
+        get {
+            return true
+        }
+    }
+    
+    // function to workwith motion
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        // If motion is detected
+        if motion == .motionShake {
+            // Set answer from list
+            setAnswer()
+        }
+    }
+    
+    // Set answer from string
+    func setAnswer(answer: String) {
+        // Set label text to answer string
+        answerTextLabel.text = answer
+    }
+    
+    // Set answer from hardcoded array of strings
+    func setAnswer() {
+        // Set label text to random hardcoded answer
+        answerTextLabel.text = answersHardcoded.randomElement()
+    }
+    
+    // Array for hardcoded answers
+    let answersHardcoded = ["Yes",
+                            "No",
+                            "No doubt about it",
+                            "Absolutely",
+                            "The stars say no",
+                            "So it shall be",
+                            "Unlikely",
+                            "Indications say yes",
+                            "Positively",
+                            "You can count on it",
+                            "Answer unclear ask later",
+                            "Don't bet on it",
+                            "Looks like yes",
+                            "Chance aren't good",
+                            "Focus and ask again",
+                            "Can't say now",
+                            "Prospect good",
+                            "Very likely",
+                            "Consult me later",
+                            "Cannot foretell now",
+                            "We will be asking questions"]
+    
+}
+
+*/
