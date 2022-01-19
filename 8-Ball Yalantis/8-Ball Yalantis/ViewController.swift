f2a9ae561ee3234e9daa46dc6787a123b7289046
Yevhenii Vuksta <vuksta.yevhenii@gmail.com>
2022-01-19 09:46:54 +0200

    8-Ball: Initial commit

---
+++ 8-Ball Yalantis/8-Ball Yalantis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8-Ball Yalantis
+//
+//  Created by Yevhenii Vuksta on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
